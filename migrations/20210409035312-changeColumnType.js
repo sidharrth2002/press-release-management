8c40c1e4a726aa337891a0ec5faf72c7c8b48db8
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-04-09 11:54:09 +0800

    change datatype of title column

---
+++ migrations/20210409035312-changeColumnType.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.changeColumn('pressreleases','title', {
+      type:Sequelize.TEXT
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
